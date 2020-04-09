11e5020f93f524950837f826b5276aba6e06fc57
dotun peter <oyelakindotun@gmail.com>
2020-04-09 05:51:43 +0100

    solve string: alternating characters algorithm problem

---
+++ alternating_characters.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the alternatingCharacters function below.
+def alternatingCharacters(s):
+    delete = 0
+    for i in range(len(s)):
+        try:
+            if s[i] == s[i+1]:
+                delete += 1
+        except:
+            break
+    return delete
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
