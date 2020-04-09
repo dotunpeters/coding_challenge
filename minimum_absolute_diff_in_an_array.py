77352168969e746539a2b788cfc58bd5c9264b6c
dotun peter <oyelakindotun@gmail.com>
2020-04-09 08:16:33 +0100

    abs diff algorithm problem

---
+++ minimum_absolute_diff_in_an_array.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    arr.sort()
+    return min([abs(arr[i] - arr[i+1]) for i in range(len(arr)-1)])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
